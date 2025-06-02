d2406560b89c62b58e028cf8ee996513a0cc79af
ricepotato <sukjun40@naver.com>
2025-06-02 16:49:46 +0900

    chore: update manifest version to 3 and add permissions; enhance content script with quiz handling and button interactions

--- README.md
+++ README.md
@@ -11,3 +11,20 @@
 1. 새 탭에서 chrome://extensions 를 입력하여 확장 프로그램 페이지로 이동합니다. (기본적으로 chrome:// URL은 연결할 수 없습니다.) 또는 확장 프로그램 메뉴 퍼즐 버튼을 클릭하고 메뉴 하단에서 확장 프로그램 관리를 선택합니다. 또는 Chrome 메뉴를 클릭하고 도구 더보기 위로 마우스를 가져간 다음 확장 프로그램을 선택합니다.
 2. 개발자 모드 옆에 있는 전환 스위치를 클릭하여 개발자 모드를 사용 설정합니다.
 3. 압축 해제된 항목 로드 버튼을 클릭하고 확장 프로그램 디렉터리를 선택합니다.
+
+```
+#frm > div > div.popup-container > div.btn-group.bottom
+
+
+```
+
+```
+<div class="btn-group bottom">
+    <!-- <a href="javascript:void(0);" class="btn primary md" onclick="fnSessionExtend()" title="세션연장">
+        <span>세션연장</span>
+    </a> -->
+    <a href="javascript:void(0);" class="btn primary md" onclick="fnClose()" title="학습저장 후 닫기">
+        <span id="spanFinish">학습완료</span>
+    </a>
+</div>
+```
--- scripts/content.js
+++ scripts/content.js
@@ -4,29 +4,48 @@ function init() {
 }
 
 function initInterval() {
-  hIntv = setInterval(() => {
-    console.log("checking for next button...");
-    const contentsFrame = document.getElementById("contentsFrame");
-    if (contentsFrame === null) {
-      console.warn("contentsFrame is null. skip...");
-      return;
-    }
+  hIntv = setInterval(intervalFunc, 5000);
+}
 
-    const frame = contentsFrame.contentWindow.document.getElementById("frame");
-    if (frame === null) {
-      console.warn("frame is null. skip...");
-      return;
-    }
+function intervalFunc() {
+  console.log("checking for next button...");
+  const contentsFrame = document.getElementById("contentsFrame");
+  if (contentsFrame === null) {
+    console.warn("contentsFrame is null. skip...");
+    return;
+  }
 
-    const btn = frame.contentWindow.document.getElementById("nextBtn");
-    if (btn === null) {
-      console.warn("can't find next button. skip...");
-      return;
-    }
+  const frame = contentsFrame.contentWindow.document.getElementById("frame");
+  if (frame === null) {
+    console.warn("frame is null. skip...");
+    return;
+  }
 
-    console.log("next button found. click...");
-    btn.click();
-  }, 5000);
+  // 중간에 나오는 다음 버튼튼
+  const sumNextBtn = frame.contentWindow.document.getElementById("sum_nextBtn");
+  if (sumNextBtn === null) {
+    console.warn("sum_nextBtn is null. skip...");
+  } else {
+    sumNextBtn.click();
+  }
+
+  const finishBtn = finishButton(document);
+  if (finishBtn !== null) {
+    console.log("finish button found. click...");
+    finishBtn.click();
+    return;
+  } else {
+    console.warn("finish button not found. skip...");
+  }
+
+  const btn = frame.contentWindow.document.getElementById("nextBtn");
+  if (btn === null) {
+    console.warn("can't find next button. skip...");
+    return;
+  }
+
+  console.log("next button found. click...");
+  btn.click();
 }
 
 function addNotiBanner() {
@@ -41,6 +60,92 @@ function bannerElement() {
   banner.style =
     "display: flex; align-items: center; justify-content: center; position: absolute; top: 0; right: 0; width: 250px; height: 25px; background-color: #808080; color: white;";
   return banner;
+}
+
+function quiz() {
+  const innerDocument = getInnerDocument();
+
+  const quizCheckBtn = innerDocument.getElementById("multipleQuiz_checkBtn");
+  if (quizCheckBtn !== null) {
+    console.log("quiz check phase...");
+    console.log(quizData);
+    return;
+  }
+
+  const quizStartBtn = innerDocument.getElementById("quiz_start_btn");
+  if (quizStartBtn !== null) {
+    quizStartBtn.click();
+  }
+
+  const quizNextBtn = innerDocument.getElementById("multipleQuiz_nextBtn");
+  if (quizNextBtn !== null) {
+    quizNextBtn.click();
+  }
+}
+
+function getInnerDocument() {
+  const contentsFrame = document.getElementById("contentsFrame");
+  if (contentsFrame === null) {
+    console.warn("contentsFrame is null. skip...");
+    return;
+  }
+
+  const frame = contentsFrame.contentWindow.document.getElementById("frame");
+  if (frame === null) {
+    console.warn("frame is null. skip...");
+    return;
+  }
+
+  return frame.contentWindow.document;
+}
+
+function solveQuiz() {
+  setInterval(() => {
+    const questionNumber = getQuestionNumber();
+    console.log(questionNumber);
+    console.log(quizData);
+  }, 5000);
+}
+
+function getQuestionNumber() {
+  let rtn = 1;
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  numbers.forEach((number) => {
+    const question = document.getElementsByClassName(`q${number}`);
+    if (question.length > 0) {
+      rtn = number;
+      return;
+    }
+  });
+  return null;
+}
+
+/**
+ * <div class="btn-group bottom">
+    <!-- <a href="javascript:void(0);" class="btn primary md" onclick="fnSessionExtend()" title="세션연장">
+        <span>세션연장</span>
+    </a> -->
+    <a href="javascript:void(0);" class="btn primary md" onclick="fnClose()" title="학습저장 후 닫기">
+        <span id="spanFinish">학습완료</span>
+    </a>
+</div>
+
+ * @param {*} contentDocument 
+ * @returns 
+ */
+function finishButton(contentDocument) {
+  //   const selector =
+  //     "#frm > div > div.popup-container > div.btn-group.bottom > a";
+  //   const btn = contentDocument.querySelector(selector);
+
+  const btn = contentDocument.getElementById("spanFinish");
+  if (btn.innerHTML === "학습완료") {
+    return btn;
+  }
+  if (btn !== null) {
+    console.warn("finish button found. but not match. skip...");
+  }
+  return null;
 }
 
 init();
